63532aa066dda6442f488397b7f7819ba35f6e5c
IvanDetkov <ivandetkov.job@gmail.com>
2026-07-24 23:31:44 +0300

    Code improvements and functionality updates

--- main.go
+++ main.go
@@ -4,49 +4,79 @@ import "fmt"
 
 func main() {
 	// input of the first number with validation
-	var num1 float64
-	fmt.Print("Enter the first number: ")
-	_, err := fmt.Scan(&num1)
-	if err != nil {
-		fmt.Println("Error! Enter a number, not text!")
-		return
-	}
-
 	// input of the second number with validation
-	var num2 float64
-	fmt.Print("Enter the second number: ")
-	_, err = fmt.Scan(&num2)
-	if err != nil {
-		fmt.Println("Error! Enter a number, not text!")
-		return
-	}
+	for {
+		var (
+			a      float64
+			b      float64
+			c      string
+			result float64
+			choice int
+		)
 
-	// input of the operator with validation
-	var mark string
-	fmt.Print("Enter the operator (+, -, *, /): ")
-	fmt.Scan(&mark)
-	if mark != "+" && mark != "-" && mark != "*" && mark != "/" {
-		fmt.Printf("Error! Invalid operator: %v\n", mark)
-	}
+		fmt.Print("Enter the first number: ")
+		a = getNumber()
 
-	// calculations
-	var result float64
-	switch mark {
-	case "+":
-		result = num1 + num2
-		fmt.Printf("Answer: %.2f + %.2f = %.2f\n", num1, num2, result)
-	case "-":
-		result = num1 - num2
-		fmt.Printf("Answer: %.2f - %.2f = %.2f\n", num1, num2, result)
-	case "/":
-		if num2 == 0 {
-			fmt.Println("Error! Division by zero is impossible!")
-			return
+		fmt.Print("Enter the second number: ")
+		b = getNumber()
+
+		fmt.Print("Enter the operator (+, -, *, /): ")
+		c = getMark()
+
+		// calculations
+		switch c {
+		case "+":
+			result = a + b
+		case "-":
+			result = a - b
+		case "/":
+			if b == 0 {
+				fmt.Println("Error! Division by zero is impossible!")
+				return
+			}
+			result = a / b
+		case "*":
+			result = a * b
+		default:
+			fmt.Println("Error! Invalid mark!")
 		}
-		result = num1 / num2
-		fmt.Printf("Answer: %.2f / %.2f = %.2f\n", num1, num2, result)
-	case "*":
-		result = num1 * num2
-		fmt.Printf("Answer: %.2f * %.2f = %.2f\n", num1, num2, result)
+		fmt.Printf("Answer: %.2f %v %.2f = %.2f\n", a, c, b, result)
+
+		fmt.Println("Do you want to continue? 1 - Yes, 2 - No")
+		fmt.Scan(&choice)
+
+		switch choice {
+		case 1:
+			continue
+		case 2:
+			fmt.Println("Bye!")
+			return
+		default:
+			fmt.Println("Error! Invalid number!")
+		}
+
+	}
+}
+
+//getNumbers gets user input
+func getNumber() float64 {
+	var num float64
+	for {
+		_, err := fmt.Scan(&num)
+		if err == nil {
+			return num
+		}
+		fmt.Println("Error! Enter a number, not text!")
+	}
+}
+
+func getMark() string {
+	var mark string
+	for {
+		_, err := fmt.Scan(&mark)
+		if err == nil {
+			return mark
+		}
+		fmt.Println("Error: please enter a symbol!")
 	}
 }
