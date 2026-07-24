04e38b78cde3389cfb7a6f25c373436691bf5561
IvanDetkov <ivandetkov.job@gmail.com>
2026-07-24 15:20:44 +0300

    fix code

--- main.go
+++ main.go
@@ -3,50 +3,50 @@ package main
 import "fmt"
 
 func main() {
-	// ввод первого числа с проверкой
+	// input of the first number with validation
 	var num1 float64
-	fmt.Print("Введите первое число: ")
+	fmt.Print("Enter the first number: ")
 	_, err := fmt.Scan(&num1)
 	if err != nil {
-		fmt.Println("Ошибка! Введите число, а не текст!")
+		fmt.Println("Error! Enter a number, not text!")
 		return
 	}
 
-	// ввод второго числа с проверкой
+	// input of the second number with validation
 	var num2 float64
-	fmt.Print("Введите второе число: ")
+	fmt.Print("Enter the second number: ")
 	_, err = fmt.Scan(&num2)
 	if err != nil {
-		fmt.Println("Ошибка! Введите число, а не текст!")
+		fmt.Println("Error! Enter a number, not text!")
 		return
 	}
 
-	// ввод знака с проверкой
+	// input of the operator with validation
 	var mark string
-	fmt.Print("Введите знак (+, -, *, /): ")
+	fmt.Print("Enter the operator (+, -, *, /): ")
 	fmt.Scan(&mark)
 	if mark != "+" && mark != "-" && mark != "*" && mark != "/" {
-		fmt.Printf("Ошибка! Неверный знак: %v\n", mark)
+		fmt.Printf("Error! Invalid operator: %v\n", mark)
 	}
 
-	// вычесления
+	// calculations
 	var result float64
 	switch mark {
 	case "+":
 		result = num1 + num2
-		fmt.Printf("Ответ: %.2f + %.2f = %.2f\n", num1, num2, result)
+		fmt.Printf("Answer: %.2f + %.2f = %.2f\n", num1, num2, result)
 	case "-":
 		result = num1 - num2
-		fmt.Printf("Ответ: %.2f - %.2f = %.2f\n", num1, num2, result)
+		fmt.Printf("Answer: %.2f - %.2f = %.2f\n", num1, num2, result)
 	case "/":
 		if num2 == 0 {
-			fmt.Println("Ошибка! Деление на ноль невозможно!")
+			fmt.Println("Error! Division by zero is impossible!")
 			return
 		}
 		result = num1 / num2
-		fmt.Printf("Ответ: %.2f / %.2f = %.2f\n", num1, num2, result)
+		fmt.Printf("Answer: %.2f / %.2f = %.2f\n", num1, num2, result)
 	case "*":
 		result = num1 * num2
-		fmt.Printf("Ответ: %.2f * %.2f = %.2f\n", num1, num2, result)
+		fmt.Printf("Answer: %.2f * %.2f = %.2f\n", num1, num2, result)
 	}
 }
